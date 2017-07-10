31a57e5910b69b60d07bbbc420b0f00dc355fa76
kofsb@qq.com <kofsb@qq.com>
2017-07-10 18:42:44 +0800

    third

--- src/test/java/com/asiainfo/chapter1/luomin/Hello.java
+++ src/test/java/com/asiainfo/chapter1/luomin/Hello.java
@@ -3,7 +3,7 @@ package test.java.com.asiainfo.chapter1.luomin;
 public class Hello {
 	
 	public static void main(String[] args ){
-		System.out.println("Hello World! version1");
+		System.out.println("Hello World! version2");
 	}
 
 }
