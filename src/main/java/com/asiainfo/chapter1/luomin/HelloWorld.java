98f2d912bd92af01001f9a5daf076482b7b50e8d
luomin23 <luomin@asiainfo.com>
2017-07-11 10:34:11 +0800

    学习环境搭建test

---
+++ src/main/java/com/asiainfo/chapter1/luomin/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.asiainfo.chapter1.luomin;
+
+/**
+ * @author Luoo
+ * @create 2017-07-10 18:26
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+    }
+}
