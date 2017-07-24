ab7f15b421e451ab0ea34ae110925c1baf8ae50c
wjz001 <1123053342@qq.com>
2017-07-24 12:56:38 +0800

    test

---
+++ src/main/java/com/asiainfo/chapter1/luomin/wangjz/java基础/test.java
@@ -0,0 +1,14 @@
+package com.asiainfo.chapter1.luomin.wangjz.java基础;
+
+import org.junit.Test;
+
+/**
+ * Created by wjz123456 on 2017/7/24.
+ */
+public class test {
+@Test
+    public void test(){
+        String a[]=new String[]{"1","2","wjz","aa"};
+        System.out.println(a.length);
+    }
+}
